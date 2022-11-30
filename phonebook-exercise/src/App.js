9f096f97ab53083a76f94d5c7857b9ccd7261167
Vuur Mos <116036135+VuurMos@users.noreply.github.com>
2022-11-30 20:00:12 +1300

    Completed exercise 2.11

--- phonebook-exercise/src/App.js
+++ phonebook-exercise/src/App.js
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Filter = (props) => {
   return (
@@ -58,15 +59,24 @@ const PersonDisplay = ({ persons, searchFilter }) => {
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]) 
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [searchFilter, setSearchFilter] = useState('')
+
+  const hook = () => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }
+
+  useEffect(hook, [])
+
+  console.log('render', persons.length, 'persons')
 
   const handleSearchFilter = (event) => {
     setSearchFilter(event.target.value)
