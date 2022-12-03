a3b6b6cd6c027469ee540777bf1e8e10dd2ff19a
Vuur Mos <116036135+VuurMos@users.noreply.github.com>
2022-12-03 17:54:35 +1300

    Completed exercise 2.14

--- countries-data-exercise/src/App.js
+++ countries-data-exercise/src/App.js
@@ -11,7 +11,9 @@ const CountrySearch = ({ handleFilter }) => {
 }
 
 const Results = ({ countries, searchFilter, viewCountry }) => {
-  const filteredCountries = countries.filter(country => country.name.common.toLowerCase().includes(searchFilter.toLowerCase()))
+  const filteredCountries = countries.filter(country => 
+    country.name.common.toLowerCase().includes(searchFilter.toLowerCase())
+  )
 
   if (filteredCountries.length === 1) {
     return (
@@ -29,7 +31,11 @@ const Results = ({ countries, searchFilter, viewCountry }) => {
     return (
       <div>
         <ul>
-          {filteredCountries.map(country => <div key={country.name.official}>{country.name.common}<button onClick={() => viewCountry(country.name.common)}>Show</button></div>)}
+          {filteredCountries.map(country => 
+            <div key={country.name.official}>
+              {country.name.common}
+              <button onClick={() => viewCountry(country.name.common)}>Show</button>
+            </div>)}
         </ul>
       </div>
     )
@@ -37,7 +43,15 @@ const Results = ({ countries, searchFilter, viewCountry }) => {
 }
 
 const ExpandedView = ({ selectedCountry }) => {
-  const languages = Object.values(selectedCountry.languages)
+  const [weather, setWeather] = useState(null)
+  const capital = selectedCountry.capital
+
+  useEffect(() => {
+    const api_key = process.env.REACT_APP_API_KEY
+    axios
+      .get(`https://api.openweathermap.org/data/2.5/weather?q=${capital}&appid=${api_key}`)
+      .then((response) => setWeather(response.data))
+  }, [capital])
 
   return (
     <div>
@@ -47,12 +61,28 @@ const ExpandedView = ({ selectedCountry }) => {
       <div>
         <strong>languages: </strong>
         <ul>
-          {languages.map(language => <div key={language}>{language}</div>)}
+          {Object.values(selectedCountry.languages).map(language => <div key={language}>{language}</div>)}
         </ul>
       </div>
       <img src={selectedCountry.flags.png}></img>
+      <WeatherInfo weather={weather} />
     </div>
   )
+}
+
+const WeatherInfo = ({ weather }) => {
+  if (weather != null) {
+    const icon = `https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`
+
+    return (
+      <div>
+        <h1>Weather in {weather.name}</h1>
+        <p>temperature: {weather.main.temp} C</p>
+        <img src={icon}></img>
+        <p>wind: {weather.wind.speed} m/s</p>
+      </div>
+    )
+  }
 }
 
 function App() {
