2ffb9dbf1e83e5f99ec5d2bf5f27ab1105bb4f6f
Vuur Mos <116036135+VuurMos@users.noreply.github.com>
2022-12-05 09:15:40 +1300

    Completed exercise 2.19

--- phonebook-exercise/src/App.js
+++ phonebook-exercise/src/App.js
@@ -3,12 +3,15 @@ import Filter from './components/Filter'
 import PersonDisplay from './components/PersonDisplay'
 import PersonForm from './components/PersonForm'
 import personService from './services/personService'
+import Notification from './components/Notification'
+import './index.css'
 
 const App = () => {
   const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [searchFilter, setSearchFilter] = useState('')
+  const [notificationMessage, setNotificationMessage] = useState(null)
 
   useEffect(() => {
     personService
@@ -30,6 +33,13 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const updateNotificationMessage = (message) => {
+    setNotificationMessage(message)
+    setTimeout(() => {
+      setNotificationMessage(null)
+    }, 5000)
+  }
+
   const addName = (event) => {
     event.preventDefault()
     const existingResult = persons.find(person => person.name === newName)
@@ -45,6 +55,7 @@ const App = () => {
         .then(returnedPerson => {
           setPersons(persons.concat(returnedPerson))
           setNewName('')
+          updateNotificationMessage(`Added ${newName}`)
         })
     } else {
       if (window.confirm(`${newName} is already added to phonebook, replace the old number with a new one?`)) {
@@ -52,6 +63,7 @@ const App = () => {
         const person = persons.find(person => person.id === id)
         const updatedPerson = { ...person, number: newNumber }
         updateEntry(id, updatedPerson)
+        updateNotificationMessage(`${newName}'s number has been updated`)
       }
     }
   }
@@ -78,6 +90,7 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification message={notificationMessage} />
       <Filter handleSearchFilter={handleSearchFilter} />
 
       <h2>add a new</h2>
